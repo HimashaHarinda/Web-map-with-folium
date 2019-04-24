01d57046f2bcb12208c013489522b03cfb97c7ff
Himasha Kapugeekiyanage <himasha.kapugeekiyanage@ifsworld.com>
2019-04-24 17:09:42 +0530

    added a marker with the current location of the user with the IP address

--- webMap.py
+++ webMap.py
@@ -1,5 +1,7 @@
 import folium
+import geocoder
 
-map = folium.Map(location=[38.58,-9989],zoom_start=6,tiles="Mapbox bright")
-
+ip = geocoder.ip('me')
+map = folium.Map(location=ip.latlng)
+map.add_child(folium.Marker(location=ip.latlng,popup="Marker",icon=folium.Icon(color='blue')))
 map.save("webMap.html")
